17c92ae691229a138b7d208daf83e4e2e34a660a
TravisH18 <travish1842@gmail.com>
2023-10-09 17:21:13 -0700

    Load CSV data into X, Y for train-test-split

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
+from sklearn.model_selection import train_test_split
+
 
 #define model
 model = keras.Sequential([
@@ -11,4 +13,17 @@ model = keras.Sequential([
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 #Load data
+# Assuming 'MVP' is your target variable and you want to predict it
+df1 = pd.read_csv('2018_batting.csv')
+df2 = pd.read_csv('2019_batting.csv')
+df3 = pd.read_csv('2020_batting.csv')
+df4 = pd.read_csv('2021_batting.csv')
+df5 = pd.read_csv('2022_batting.csv')
+combined_df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
+X = combined_df.drop('MVP', axis=1)  # Features
+y = combined_df['MVP']  # Target variable
+
+# Split the data into a training set (80%) and a testing set (20%)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))

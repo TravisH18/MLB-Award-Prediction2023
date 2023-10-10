b9ac6da726139312e0f0c688eaea77d601673d12
TravisH18 <travish1842@gmail.com>
2023-10-10 11:50:51 -0700

    Need to download on GPU machine to run, data prepared

--- load_data.py
+++ load_data.py
@@ -1,39 +1,42 @@
 import pandas as pd
 
-bad_columns = ['Rk', 'Name', 'Age', 'Lg', 'Tm', 'Pos Summary','Name-additional']
-df2018 = pd.read_csv('2018 MLB Player Stats - Batting.csv')
+bad_columns = ['Rk', 'Age', 'Lg', 'Tm', 'Pos Summary','Name']
+keep = ['PA', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'BB', 'SO', 'BA', 'OBP','SLG','OPS','OPS+','TB','GDP','HBP','SH','SF','IBB', 'Name-additional']
+col_types = {'PA': int, 'AB': int, 'R': int, 'H': int, '2B': int, '3B': int, 'HR': int, 'RBI': int, 'SB': int, 'CS': int, 'BB': int, 'SO': int, 'BA': float, 'OBP': float,'SLG': float,'OPS': float,'OPS+': float,'TB': float,'GDP': int,'HBP': int,'SH': int,'SF': int,'IBB': int, 'Name-additional': str}
+df2018 = pd.read_csv('2018 MLB Player Stats - Batting.csv', usecols=keep, dtype=col_types, index_col=False)
+print(df2018.head)
 df2018['MVP'] = 0
-mvp_names2018 = ['Christian Yelich*', 'Mookie Betts']
-df2018.loc[df2018['Name'].isin(mvp_names2018), 'MVP'] = 1
+mvp_names2018 = ['yelicch01', 'bettsmo01']
+df2018.loc[df2018['Name-additional'].isin(mvp_names2018), 'MVP'] = 1
 
 #before saving get rid of Rk, Name, Age, Lg, Tm, Pos Summary,Name-additional
-df2018.drop(bad_columns, axis=1)
+#df2018.drop(bad_columns, axis=1)
 df2018.to_csv('2018_batting.csv', index=False)
 
-df2019 = pd.read_csv('2019 MLB Player Stats - Batting.csv')
+df2019 = pd.read_csv('2019 MLB Player Stats - Batting.csv', usecols=keep, dtype=col_types, index_col=False)
 df2019['MVP'] = 0
-mvp_names2019 = ['Mike Trout', 'Cody Bellinger*']
-df2019.loc[df2019['Name'].isin(mvp_names2019), 'MVP'] = 1
-df2019.drop(bad_columns, axis=1)
+mvp_names2019 = ['troutmi01', 'bellico01']
+df2019.loc[df2019['Name-additional'].isin(mvp_names2019), 'MVP'] = 1
+#df2019.drop(bad_columns, axis=1)
 df2019.to_csv('2019_batting.csv', index=False)
 
-df2020 = pd.read_csv('2020 MLB Player Stats - Batting.csv')
+df2020 = pd.read_csv('2020 MLB Player Stats - Batting.csv', usecols=keep, dtype=col_types, index_col=False)
 df2020['MVP'] = 0
-mvp_names2020 = ['José Abreu', 'Freddie Freeman*']
-df2020.loc[df2020['Name'].isin(mvp_names2020), 'MVP'] = 1
-df2020.drop(bad_columns, axis=1)
+mvp_names2020 = ['abreujo02', 'freemfr01']
+df2020.loc[df2020['Name-additional'].isin(mvp_names2020), 'MVP'] = 1
+#df2020.drop(bad_columns, axis=1)
 df2020.to_csv('2020_batting.csv', index=False)
 
-df2021 = pd.read_csv('2021 MLB Player Stats - Batting.csv')
+df2021 = pd.read_csv('2021 MLB Player Stats - Batting.csv', usecols=keep, dtype=col_types, index_col=False)
 df2021['MVP'] = 0
-mvp_names2021 = ['Shohei Ohtani*', 'Bryce Harper*']
-df2021.loc[df2021['Name'].isin(mvp_names2021), 'MVP'] = 1
-df2021.drop(bad_columns, axis=1)
+mvp_names2021 = ['ohtansh01', 'harpebr03']
+df2021.loc[df2021['Name-additional'].isin(mvp_names2021), 'MVP'] = 1
+#df2021.drop(bad_columns, axis=1)
 df2021.to_csv('2021_batting.csv', index=False)
 
-df2022 = pd.read_csv('2022 MLB Player Stats - Batting.csv')
+df2022 = pd.read_csv('2022 MLB Player Stats - Batting.csv', usecols=keep, dtype=col_types, index_col=False)
 df2022['MVP'] = 0
-mvp_names2022 = ['Aaron Judge', 'Paul Goldschmidt']
-df2022.loc[df2022['Name'].isin(mvp_names2022), 'MVP'] = 1
-df2022.drop(bad_columns, axis=1)
+mvp_names2022 = ['judgeaa01', 'goldspa01']
+df2022.loc[df2022['Name-additional'].isin(mvp_names2022), 'MVP'] = 1
+#df2022.drop(bad_columns, axis=1)
 df2022.to_csv('2022_batting.csv', index=False)
--- train.py
+++ train.py
@@ -1,29 +1,50 @@
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 
-#define model
-model = keras.Sequential([
-    keras.layers.Dense(64, activation='relu', input_shape=(num_features,)),
-    keras.layers.Dense(32, activation='relu'),
-    keras.layers.Dense(1, activation='sigmoid')
-])
+def get_model():
+    model = keras.Sequential([
+        keras.layers.Dense(64, activation='relu', input_shape=(BATCH_SIZE, num_features)),
+        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(1, activation='sigmoid')
+    ])
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
 #Load data
-# Assuming 'MVP' is your target variable and you want to predict it
+# 'MVP' is your target variable and you want to predict it
 df1 = pd.read_csv('2018_batting.csv')
+#df1.drop(columns=['Rk', 'Name', 'Age', 'Lg', 'Tm', 'Pos Summary','Name-additional'], axis=1)
 df2 = pd.read_csv('2019_batting.csv')
 df3 = pd.read_csv('2020_batting.csv')
 df4 = pd.read_csv('2021_batting.csv')
 df5 = pd.read_csv('2022_batting.csv')
+#print(df1.head)
 combined_df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
-X = combined_df.drop('MVP', axis=1)  # Features
-y = combined_df['MVP']  # Target variable
+combined_df = combined_df.dropna()
+#print(combined_df.head)
 
+X = combined_df.drop(['MVP', 'Name-additional'], axis=1)  # Features
+#X = X.drop('Name-additional', axis=1)
+#print(X.head)
+#could use X.values
+X = tf.convert_to_tensor(X.values)
+#y = combined_df['MVP']  # Target variable
+y = combined_df.pop('MVP')
+key = combined_df['Name-additional']
+BATCH_SIZE = 32
+EPOCHS = 50
+num_features = X.shape[1]
+
+#define normalizer
+normalizer = tf.keras.layers.Normalization(axis=-1)
+normalizer.adapt(X)
+
+#define model
+model = get_model()
 # Split the data into a training set (80%) and a testing set (20%)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+model.fit(X, y, epochs=EPOCHS, batch_size=BATCH_SIZE)
